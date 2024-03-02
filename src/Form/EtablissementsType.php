e9fb2cbb3d6584bc0d0b24a3142beab77b2323be
WaelDridi15 <wael.dridi@esprit.tn>
2024-03-02 01:00:29 +0100

    recaptcha final

--- src/Form/EtablissementsType.php
+++ src/Form/EtablissementsType.php
@@ -8,6 +8,7 @@ use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Karser\Recaptcha3Bundle\Form\Recaptcha3Type;
 
 class EtablissementsType extends AbstractType
 {
@@ -17,9 +18,15 @@ class EtablissementsType extends AbstractType
             ->add('nom')
             ->add('categorie')
             ->add('adress')
-            ->add('mail' , EmailType::class, ['label'=>'Email'])
-            ->add('tel' , IntegerType::class, ['label'=>'Numéro De Téléphone'] )
-        ;
+            ->add('mail', EmailType::class, ['label' => 'Email'])
+            ->add('tel', IntegerType::class, ['label' => 'Numéro De Téléphone'])
+            ->add('recaptcha', Recaptcha3Type::class, [
+                'constraints' => [
+                    new \Symfony\Component\Validator\Constraints\NotBlank(),
+                    new \Symfony\Component\Validator\Constraints\Length(['min' => 3]),
+                ],
+                'action_name' => 'app_etablissements_index', // Remplacez 'contact' par le nom de votre action
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
